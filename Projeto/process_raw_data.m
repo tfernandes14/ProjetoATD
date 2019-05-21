4bbe9132acf7da87b2441ab8dc41c3df990ae7ae
Tiago <fernandes.tiago36@gmail.com>
2019-05-21 22:40:06 +0100

    Feito até ao exercício 4.1

--- Projeto/process_raw_data.m
+++ Projeto/process_raw_data.m
@@ -8,6 +8,7 @@ Sensors={'ACC_X','ACC_Y','ACC_Z'};
 activities={'W','WU','WD','S','ST','L','STSit','SitTS','SitTL','LTSit','STL','LTS'};
 Fs=50;  % Frequencia da amostragem
 
+% -------------------- EXERCÍCIO 2 --------------------
 dacc=read_raw_data('acc_exp23_user11.txt');
 
 % Le o ficheiro das labels
@@ -21,6 +22,8 @@ data=dacc;
 t=(0:size(data,1)-1)./Fs;
 
 [n_points,n_plots]=size(data);
+
+% -------------------- EXERCÍCIO 3 --------------------
 
 aac_x = cell(1, numel(ix_labels));
 aac_y = cell(1, numel(ix_labels));
@@ -58,28 +61,21 @@ for i=1:n_plots
     hold off
 end
 
-disp("Valores DFT de AAC_X:")
-aac_x
-disp("Valores DFT de AAC_Y:")
-aac_y
-disp("Valores DFT de AAC_Z:")
-aac_z
-
+% -------------------- EXERCÍCIO 4 --------------------
 
 figure(2)
 for i=1:numel(ix_labels)
-    N= length(aac_x{1,i});
-   
+   N= length(aac_x{1,i});
    if (mod(N,2) == 0)
         f = -Fs/2:Fs/N:Fs/2-Fs/N;
    else
         f = -Fs/2+Fs/(2*N):Fs/N:Fs/2-Fs/(2*N);
-    end
-    plot(f, aac_x{1,i})
-    hold on
+   end
+   plot(f, aac_x{1,i})
+   title("Frequências da DFT com a janela retangular")
+   hold on
 end
 
-figure(3)
 %varias janelas
 
 aac_x_janelas = cell(1, numel(ix_labels));
@@ -107,7 +103,83 @@ for i=1:n_plots
         end
     end
 end
+
 figure(3)
+for i=1:numel(ix_labels)
+    N= length(aac_x_janelas{1,i});
+   
+   if (mod(N,2) == 0)
+        f = -Fs/2:Fs/N:Fs/2-Fs/N;
+   else
+        f = -Fs/2+Fs/(2*N):Fs/N:Fs/2-Fs/(2*N);
+    end
+    plot(f, aac_x_janelas{1,i})
+    title("Janela de Hamming")
+    hold on
+end
+
+figure(4)
+for i=1:n_plots
+    for j=1:numel(ix_labels)    
+        %fazer a concatenaçao dos zeros estamos a aumentar o padding e
+        %assim as curvas tornam-se mais suaves o que permite uma melhor
+        %comparação que ira ser usasda para o relatorio  ps: nao esquecer
+        %que é necessario fazer À dft tambem a comcatenaçao do array de
+        %zeros como mostra o exemplo abaixo
+        
+        %NAO METER OS GRAFICOS TODOS JUNTOS TEMOS DE SEPARA-LOS MAS TAMBEM
+        %NAO PERCISAMOS DE MOSTRAR TODOS SO OS DO MOVIMENTO
+        aux = abs(fftshift(fft([zeros(1000,1);(blackman(all_labels(ix_labels(j),5) -all_labels(ix_labels(j),4)+1).*data(all_labels(ix_labels(j),4):all_labels(ix_labels(j),5),i))])));
+        if i == 1
+            aac_x_janelas{1, j} = aux;
+        end
+        if i == 2
+            aac_y_janelas{1, j} = aux;
+        end
+        if i == 3
+            aac_z_janelas{1, j} = aux;
+        end
+    end
+end
+
+
+for i=1:numel(ix_labels)
+    N= length(aac_x_janelas{1,i});
+   
+   if (mod(N,2) == 0)
+        f = -Fs/2:Fs/N:Fs/2-Fs/N;
+   else
+        f = -Fs/2+Fs/(2*N):Fs/N:Fs/2-Fs/(2*N);
+    end
+    plot(f, aac_x_janelas{1,i})
+    hold on
+end
+
+figure(5)
+for i=1:n_plots
+    for j=1:numel(ix_labels)    
+        %fazer a concatenaçao dos zeros estamos a aumentar o padding e
+        %assim as curvas tornam-se mais suaves o que permite uma melhor
+        %comparação que ira ser usasda para o relatorio  ps: nao esquecer
+        %que é necessario fazer À dft tambem a comcatenaçao do array de
+        %zeros como mostra o exemplo abaixo
+        
+        %NAO METER OS GRAFICOS TODOS JUNTOS TEMOS DE SEPARA-LOS MAS TAMBEM
+        %NAO PERCISAMOS DE MOSTRAR TODOS SO OS DO MOVIMENTO
+        aux = abs(fftshift(fft([zeros(1000,1);(hann(all_labels(ix_labels(j),5) -all_labels(ix_labels(j),4)+1).*data(all_labels(ix_labels(j),4):all_labels(ix_labels(j),5),i))])));
+        if i == 1
+            aac_x_janelas{1, j} = aux;
+        end
+        if i == 2
+            aac_y_janelas{1, j} = aux;
+        end
+        if i == 3
+            aac_z_janelas{1, j} = aux;
+        end
+    end
+end
+
+
 for i=1:numel(ix_labels)
     N= length(aac_x_janelas{1,i});
    
