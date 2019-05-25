2c4da3e2a7c54ce734615b4328b451463a721562
ViegasMP <39434577+ViegasMP@users.noreply.github.com>
2019-05-25 17:43:46 +0100

    media dos passos

--- Projeto/passos.m
+++ Projeto/passos.m
@@ -1,27 +1,36 @@
-function passos(valores)
-
-for i=1:3
-    j=1;
-    if i==1
-        disp('Walking')
+function passos(valores, n)
+    movimentos_dinamicos = 3;
+    passos_val = cell(3, n);
+    contador = [1 1 1];
+    media = zeros(1,3);
+    for i=1:movimentos_dinamicos
+        j=1;
+%         if i==1
+%             disp('Walking')
+%         end
+%         if i==2
+%             disp('Walking Up')
+%         end
+%         if i==3
+%             disp('Walking Down')
+%         end
+        while(isempty(valores{i,j})~=1)
+            pks = findpeaks(valores{i,j});
+            passos_por_minuto = pks(1)*60; %primeiro valor do pico *60
+            passos_val{i,contador(i)}=passos_por_minuto;
+            contador(i) = contador(i)+1;
+            j=j+1;
+        end
     end
-    if i==2
-        disp('Walking Up')
+    
+    for i=1:movimentos_dinamicos
+         soma=0;
+        j=1;
+        while(isempty(passos_val{i,j})~=1)
+            soma = soma+passos_val{i,j};
+            j = j+1;
+        end
+        media(i)= soma/j;
     end
-    if i==3
-        disp('Walking Down')
-    end
-    while(isempty(valores{i,j})~=1)
-        pks = findpeaks(valores{i,j});
-        disp(j)
-        %disp(max(pks))
-        %disp(max(pks(pks<max(pks)))) %segundo maior
-        disp(pks(1))%primeiro valor
-        disp(pks(1)*60)%primeiro valor do pico *60
-        j=j+1;
-    end
+  media
 end
-
-
-end
-
